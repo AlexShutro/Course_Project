2b5e53e95a79bf8880dc642a0ff2bb650b779f17
AlexShutro <117032622+AlexShutro@users.noreply.github.com>
2023-05-10 10:19:25 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,131 @@
+let a=document.getElementById("photo1")
+   a.onmouseover=function()
+   {
+    a.src="./src/covers/astro.png";
+   }
+    let b=document.getElementById("photo1")
+    b.onmouseout=function()
+{
+    b.src="./src/covers/Travis_Scott_Astroworld.png"
+}
+
+let c=document.getElementById("photo2")
+   c.onmouseover=function()
+   {
+    c.src="./src/covers/igor2.png";
+   }
+    let d=document.getElementById("photo2")
+    d.onmouseout=function()
+{
+    d.src="./src/covers/igor.jpg"
+}
+
+let e=document.getElementById("photo3")
+   e.onmouseover=function()
+   {
+    e.src="./src/covers/x.png";
+   }
+    let f=document.getElementById("photo3")
+    f.onmouseout=function()
+{
+    f.src="./src/covers/XXXTentacion.jpg"
+}
+
+   let g=document.getElementById("photo4")
+   g.onmouseover=function()
+   {
+    g.src="./src/covers/Elecric.png";
+   }
+    let h=document.getElementById("photo4")
+    h.onmouseout=function()
+{
+    h.src="./src/covers/Jimi_hendrix.jpg";
+}
+
+let i=document.getElementById("photo5")
+   i.onmouseover=function()
+   {
+    i.src="./src/covers/thriller2.jpg";
+   }
+    let j=document.getElementById("photo5")
+    j.onmouseout=function()
+{
+    j.src="./src/covers/Thriller.jpg"
+}
+
+let k=document.getElementById("photo6")
+   k.onmouseover=function()
+   {
+    k.src="./src/covers/808.jpg";
+   }
+    let l=document.getElementById("photo6")
+    l.onmouseout=function()
+{
+    l.src="./src/covers/808s_&_Heartbreak.png"
+}
+
+let m=document.getElementById("photo7")
+   m.onmouseover=function()
+   {
+    m.src="./src/covers/Tupac.jpg";
+   }
+    let n=document.getElementById("photo7")
+    n.onmouseout=function()
+{
+    n.src="./src/covers/All_Eyez_on_Me.jpg"
+}
+
+let o=document.getElementById("photo8")
+   o.onmouseover=function()
+   {
+    o.src="./src/covers/testing2.jpg";
+   }
+    let p=document.getElementById("photo8")
+    p.onmouseout=function()
+{
+    p.src="./src/covers/Testing.jpg"
+}
+
+let q=document.getElementById("photo9")
+   q.onmouseover=function()
+   {
+    q.src="./src/covers/damn2.jpg";
+   }
+    let r=document.getElementById("photo9")
+    r.onmouseout=function()
+{
+    r.src="./src/covers/Damn.jpg"
+}
+
+let s=document.getElementById("photo11")
+   s.onmouseover=function()
+   {
+    s.src="./src/covers/nothing.jpg";
+   }
+    let t=document.getElementById("photo11")
+    t.onmouseout=function()
+{
+    t.src="./src/covers/nothing2.png"
+}
+
+let u=document.getElementById("photo13")
+   u.onmouseover=function()
+   {
+    u.src="./src/covers/afterHours2.jpg";
+   }
+    let v=document.getElementById("photo13")
+    v.onmouseout=function()
+{
+    v.src="./src/covers/After_Hours.png"
+}
+
+let w=document.getElementById("photo14")
+   w.onmouseover=function()
+   {
+    w.src="./src/covers/justice2.jpg";
+   }
+    let x=document.getElementById("photo14")
+    x.onmouseout=function()
+{
+    x.src="./src/covers/Justin_Bieber.jpg"
+}
